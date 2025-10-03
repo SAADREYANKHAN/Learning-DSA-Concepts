142f5efb5dc676c104224929d850a6940af42530
SAADREYANKHAN <saadreyanrollnumber34@gmail.com>
2025-10-03 03:41:53 -0700

    Linked List Concept of Push_Front

---
+++ push_front.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace::std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+class List{
+	Node* head;
+	Node* tail;
+	
+public:
+	List(){
+		head = tail = NULL;
+	}
+	void Push_Front(int val){
+		Node* newNode = new Node(val);
+		if(head == NULL){
+			head = tail = newNode;
+		}else{
+			newNode->next = head;
+			head = newNode;
+		}
+	}
+	
+	void Print(){
+		Node* temp = head;
+		while(temp!=NULL){
+			cout<<temp->data<<"->";
+			temp = temp->next;
+		}
+		cout<<"NULL"<<endl;
+	}
+};
+int main(){
+	
+	List LL;
+	LL.Push_Front(10);
+	LL.Push_Front(20);
+	LL.Push_Front(30);
+	
+	LL.Print();
+    return 0;
+}
